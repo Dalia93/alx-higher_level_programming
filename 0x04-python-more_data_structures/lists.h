6a0ad423eeb96eda0f676f2ae89e2a5f80417806
Dalia@93 <daliabderheem93@gmail.com>
2023-12-06 14:30:53 -0800

    Set, Dictionary

--- 0x04-python-more_data_structures/lists.h
+++ 0x04-python-more_data_structures/lists.h
@@ -21,6 +21,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+def square_matrix_simple(matrix=[]):
 
 #endif /* LISTS_H */
 
